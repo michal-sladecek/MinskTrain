81f8b0eebe7473769223d24ed0e72bd72d0121d8
Michal Sladecek <michalsladecek98@gmail.com>
2016-08-29 19:25:55 +0200

    Vytvorene linky

--- client/components/LevelIcon.js
+++ client/components/LevelIcon.js
@@ -1,11 +1,16 @@
 import React from 'react' 
+import {Link} from 'react-router'
 
 const LevelIcon = React.createClass({
     render() {
         return (
+            <Link to={`/game/${this.props.level.id}`}>
             <div className='level-icon'>
-                <p>{this.props.level.objective}</p>
+                
+                    <p>{this.props.level.objective}</p>
+                
             </div>
+            </Link>
         )
     }
 })
