67b0319e90aa6a0b09355f2814ec2be39d457c81
Michal Sladecek <michalsladecek98@gmail.com>
2016-09-06 19:39:04 +0200

    Fixed level 6

--- client/data/levels.js
+++ client/data/levels.js
@@ -3,37 +3,37 @@ import toolGroups from '../consts/items'
 const levels = {
     'levels': [
         {
-            'id': 1,
+            'id': 0,
             'objective': 'Pripocitaj jedna k prvemu vagonu',
             'hint': 'Vlak vzdy prichadza z lava na lave horne policko, a ked vyjde z mapy predpokladame ze su splnene podmienky',
             'allowed': [toolGroups.RAILS, toolGroups.ADD_ONE]
         },
         {
-            'id': 2,
+            'id': 1,
             'objective': 'Odpocitaj jedna k prvemu vagonu',
             'hint': 'Vsimni si, co si dostal :)',
             'allowed': [toolGroups.RAILS, toolGroups.ADD_ONE, toolGroups.SUB_ONE]
         },
         {
-            'id': 3,
+            'id': 2,
             'objective': 'V prvom vozni musi byt na konci 0',
             'hint': 'Po vyhybkach vlak pojde rovno, ak je v danom vozni 0, inac pojde do strany',
             'allowed': [toolGroups.RAILS, toolGroups.ADD_ONE, toolGroups.SUB_ONE, toolGroups.SWITCHES]
         },
         {
-            'id': 4,
+            'id': 3,
             'objective': 'V tretom vozni ma byt sucet prvych dvoch',
             'hint': 'Vlak vzdy prichadza z lava na lave horne policko, a ked vyjde z mapy predpokladame ze su splnene podmienky',
             'allowed': [toolGroups.RAILS, toolGroups.ADD_ONE]
         },
         {
-            'id': 5,
+            'id': 4,
             'objective': 'Vymen cisla v prvom a druhom vozni',
             'hint': 'Vsimni si, co si dostal :)',
             'allowed': [toolGroups.RAILS, toolGroups.ADD_ONE, toolGroups.SUB_ONE]
         },
         {
-            'id': 6,
+            'id': 5,
             'objective': 'Pohraj sa :)',
             'hint': 'Po vyhybkach vlak pojde rovno, ak je v danom vozni 0, inac pojde do strany',
             'allowed': [toolGroups.RAILS, toolGroups.ADD_ONE, toolGroups.SUB_ONE, toolGroups.SWITCHES]
