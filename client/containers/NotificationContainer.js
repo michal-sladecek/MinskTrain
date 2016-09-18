ae3e3a610427be8bb73260a1de99bbb17322f2ce
Michal Sladecek <michalsladecek98@gmail.com>
2016-09-18 22:22:07 +0200

    Notify

---
+++ client/containers/NotificationContainer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import {bindActionCreators} from 'redux'
+import * as actionCreators from '../actions/trainActions'
+import {Modal} from 'react-bootstrap'
+import getNotify from '../messages/errors'
+
+
+const Notification = React.createClass({
+    render(){
+        const hideModal = (e) => {
+            this.props.clearNotify()
+        }
+        return (
+            <Modal show = {this.props.notify.id !== ''} onHide={hideModal}>
+                {getNotify(this.props.notify)}
+            </Modal>
+        )
+    }
+})
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+      notify: state.game.train.notify
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(actionCreators, dispatch)
+}
+
+const NotificationContainer = connect(
+  mapStateToProps,mapDispatchToProps)(Notification)
+export default NotificationContainer
