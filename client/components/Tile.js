c1cb1e03623e4f42033b27bd6915b39bea378992
Michal Sladecek <michalsladecek98@gmail.com>
2016-09-27 20:54:59 +0200

    Drag n Drop

--- client/components/Tile.js
+++ client/components/Tile.js
@@ -4,15 +4,19 @@ import items from './items/items'
 import {Modal, OverlayTrigger, Tooltip} from 'react-bootstrap'
 import tooltips from '../messages/tooltips'
 import getModalBody from './items/forms'
+
+
 const Tile = React.createClass({
     getInitialState(){
         return { showModal: false }
     },
     render(){
-        let tileImg = []
-        if(this.props.show){
-            tileImg=items[this.props.show.type]           
+        const submitNoModal = (e) => {
+            if(e.target !== this.refs.div) return
+            e.preventDefault()
+            this.props.useTool(this.props.coord)
         }
+
         const submit = (e) => {
             e.preventDefault()
 
@@ -23,51 +27,93 @@ const Tile = React.createClass({
             this.props.useTool(this.props.coord, values)
             hideModal()
         }
-        const submitNoModal = (e) => {
-            if(e.target !== this.refs.div) return
-            e.preventDefault()
-            this.props.useTool(this.props.coord)
-        }
+        const doNothing = (e) => {e.preventDefault()}
         const showModal = (e) => {
+            console.log(this.props)
             if(e.target !== this.refs.div) return
             this.setState({ showModal: true })
         }
+
         const hideModal = (e) => {
             this.setState({ showModal: false })
         }
+        const getEvents = (onTile, currentTool) => {
+            if(currentTool === 'ERASER') {
+                return {
+                    onClick: submitNoModal,
+                    onDragEnter: submitNoModal
+                }
+            }
+            let onDragStart = ()=>{}
+            if(onTile != null) {
+                onDragStart = this.props.changeTool.bind(this,onTile)
+            }
+            if(currentTool === 'LR' || currentTool === 'UD') {
+                return {
+                    onClick: submitNoModal,
+                    onDragEnter: submitNoModal,
+                    onDragStart
+                }
+            }
+            else if(currentTool === 'UL' || currentTool === 'LD' || currentTool === 'DR' || currentTool === 'RU') {
+                return {
+                    onDragOver: doNothing,
+                    onClick: submitNoModal,
+                    onDrop: submitNoModal,
+                    onDragStart
+                }
+            }
+            else {
+                return {
+                    onDragOver: doNothing,
+                    onClick: showModal,
+                    onDrop: showModal,
+                    onDragStart
+                }
+            }
+
+            
+        }
+
+        
+        let tileImg = []
+        if(this.props.show){
+            tileImg=items[this.props.show.type]           
+        }
+        
         let form = getModalBody(submit, this.props.currentTool)
-        let modal = []
-        let whatToDo = submitNoModal
-        if(form){
+
+        let modal = null
+
+        
+        if (form) {
             modal = (
                 <Modal show={this.state.showModal} onHide={hideModal} autoFocus>
                     {form}
                 </Modal>
             )
-            whatToDo = showModal
         }
-        if(this.props.show && this.props.tooltip){
+        const body = (
+            <div ref='div' className='Tile' {...getEvents((this.props.show ? this.props.show.type : null), this.props.currentTool)} draggable>
+                { tileImg }
+                {modal}
+            </div>
+        )
+        let tooltip = null
+        if (this.props.tooltip) {
             const tooltip = (
-             <Tooltip id="tooltip" >{tooltips[this.props.show.type](this.props.show.id)}</Tooltip>
+                <Tooltip id="tooltip" >{tooltips[this.props.show.type](this.props.show.id) }</Tooltip>
             )
             return (
-            <OverlayTrigger placement="left" overlay={tooltip} draggable>
-                <div ref='div' className='Tile' onClick={whatToDo} onDragEnter={whatToDo}>
-                    { tileImg }
-                    {modal}        
-                </div>
-             </OverlayTrigger>         
-        ) 
+                <OverlayTrigger placement="left" overlay={tooltip}  >
+                    {body}
+                </OverlayTrigger>
+            )
         }
         else {
-           return (
-                <div ref='div' className='Tile' onClick={whatToDo} onDragEnter={whatToDo} draggable>
-                    { tileImg }
-                    {modal}
-                </div>  
-        )  
+            return body
         }
-        
     }
 })
+
 export default Tile
