17833fc2aa9a240f3032f567aeb7b74ccf59e370
Marián Horňák <marian.sysel.hornak@gmail.com>
2016-11-06 12:01:24 +0100

    Refactored Menu

--- client/components/LevelIcon.js
+++ client/components/LevelIcon.js
@@ -1,22 +1,24 @@
 import React from 'react' 
 import {Link} from 'react-router'
-import {Button} from 'react-bootstrap'
+import {Button, Glyphicon} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
+
 const LevelIcon = React.createClass({
-    render() {
-        return (
-            <Link to={`/game/${this.props.level.id}`}>
-            <Button block>
-            
-            <div className='level-icon'>  
-                    {this.props.level.solved? <h2>VYRIESENY</h2>:null}
-                    <h2>Level {this.props.level.id}</h2>     
-                    <p>{this.props.level.objective}</p>     
-            </div>
-            
-            </Button>
-            </Link>
-        )
-    }
+  render() {
+    return (
+      <LinkContainer to={`/game/${this.props.level.id}`}>
+        <Button style={{width: '100%'}}>
+          <h3>
+            Level {this.props.level.id}
+            <span>&nbsp;&nbsp;{this.props.level.objective.slice(0, 4)}</span>
+            {this.props.level.solved &&
+              (<span>&nbsp;&nbsp;<Glyphicon glyph="ok" /></span>)
+            }
+          </h3>
+        </Button>
+      </LinkContainer>
+    )
+  }
 })
 
 export default LevelIcon
--- client/components/Menu.js
+++ client/components/Menu.js
@@ -1,47 +1,30 @@
 import React from 'react'
 import NotImplemented from './NotImplemented'
 import LevelIcon from './LevelIcon'
-import {Row, Col} from 'react-bootstrap'
+import {Grid, Row, Col} from 'react-bootstrap'
 import {divideArray} from '../functions/helpers'
 
-const LevelRow = React.createClass({
-    render() {
-        const levels = this.props.levels
-        return (
-            
-            <div className='levels-row'>
-                <Col md={2}/>
-                {
-                    levels.map((level, i) => {
-                        return <Col md={2} key={i}><LevelIcon key={i} level={level}/></Col>
-                    })
-                }
-                <Col md={2}/>
-            </div>
-             
-        )
-    }
-})
 
 const Menu = React.createClass({
-    componentDidMount() {
-        this.props.getSolvedLevels()
-    },
-    render() {
-        const levels = Object.values(this.props.levels)
-        const levelsDivided = divideArray(levels.slice(0),4)
-        return (
-            <div className='levels-grid'>
-                {
-                    levelsDivided.map((levels, i) => {
-                        return <Row key={i}><LevelRow key={i} levels={levels} /></Row>
-                    })
-                }
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.props.getSolvedLevels()
+  },
+  render() {
+    const levels = Object.keys(this.props.levels).map((key) => this.props.levels[key])
+    return (
+      <Grid>
+        <Row>
+          {
+            levels.map((level, i) => (
+              <Col lg={3} md={4} sm={6} key={i} style={{height: 115}}>
+                <LevelIcon level={level}/>
+              </Col>
+            ))
+          }
+        </Row>
+      </Grid>
+    )
+  }
 })
-
-
 
 export default Menu
