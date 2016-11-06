f744db6e3ebc733e1987b436b729ce6cfca7bd6b
Marián Horňák <marian.sysel.hornak@gmail.com>
2016-11-06 04:42:06 +0100

    Refactor train from

--- client/components/Carriage.js
+++ client/components/Carriage.js
@@ -1,31 +1,55 @@
 import React from 'react'
 import NotImplemented from './NotImplemented'
-import {Button, Modal} from 'react-bootstrap'
+import {Modal, Button, FormGroup, ControlLabel, FormControl} from 'react-bootstrap'
 import messages from '../messages/messages'
 const Carriage = React.createClass({
-    getInitialState: function() {
-        return { show: false };
+
+    getInitialState() {
+        return { show: false, value: null }
     },
+
     render(){
         let indexChar = String.fromCharCode(this.props.id+65)
-        const submit = (e) => {
-            e.preventDefault()
-            this.props.changeNumber(this.props.id, e.target.getElementsByTagName("input")[0].value)
-            hideModal()
-        }
         const showModal = (e) => {
-            this.setState({ show: true })
+            this.setState({ ...this.state, show: true })
         }
         const hideModal = (e) => {
-            this.setState({ show: false })
+            this.setState({ ...this.state, show: false, value: null })
         }
+        const setValue = (value) => {
+            this.setState({ ...this.state, value })
+        }
+        const submit = (e) => {
+            e.preventDefault()
+            const value = this.state.value
+            this.props.changeNumber(this.props.id, value)
+            hideModal()
+        }
+        console.log('kreslim', this.state)
         return (
             <div onClick={showModal} className='Carriage'>
                 <p>{indexChar}</p>
                 <h3 >{this.props.num}</h3>
                 <Modal show={this.state.show} onHide={hideModal}>
                     <form onSubmit={submit}>
-                        <input type='text' placeholder={messages.enterNumber} autoFocus/>
+                        <Modal.Body>
+                            <FormGroup>
+                                <ControlLabel>{messages.enterNumber}</ControlLabel>
+                                <FormControl
+                                    type="number"
+                                    min="0"
+                                    max="99"
+                                    autoFocus
+                                    value={this.state.value === null ? this.props.num : this.state.value }
+                                    onChange={(e) => setValue(e.target.value)}
+                                />
+                            </FormGroup>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button type="submit">
+                                Nastav
+                            </Button>
+                        </Modal.Footer>
                     </form>
                 </Modal>
             </div>
